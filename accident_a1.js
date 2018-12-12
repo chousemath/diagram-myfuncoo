4cd2775c47111c787ed47119b07e3fe99ca7f609
Joseph Sungpil Choi <chousemath@gmail.com>
2018-12-12 13:37:56 +0900

    Quick save...

--- accident_a1.js
+++ accident_a1.js
@@ -1,14 +1,17 @@
 const nomnoml = require('nomnoml');
 
 const src = `
-[<choice> Vel > 15mph] -> [<abstract> No*]
-[<choice> Vel > 15mph] -> [<abstract> Yes*]
-[<abstract> No*] -> [Ignore]
-[<abstract> Yes*] -> [<choice> Accel > 4G]
-[<choice> Accel > 4G] -> [<abstract> No**]
-[<choice> Accel > 4G] -> [<abstract> Yes**]
-[<abstract> No**] -> [Ignore]
-[<abstract> Yes**] -> [Accident]
+#.yes: fill=#ceffcf visual=ellipse
+#.no: fill=#ffc5c5 visual=ellipse
+
+[<choice> Vel > 15mph] -> [<no> No*]
+[<choice> Vel > 15mph] -> [<yes> Yes*]
+[<no> No*] -> [Ignore]
+[<yes> Yes*] -> [<choice> Accel > 4G]
+[<choice> Accel > 4G] -> [<no> No**]
+[<choice> Accel > 4G] -> [<yes> Yes**]
+[<no> No**] -> [Ignore]
+[<yes> Yes**] -> [Accident]
 
 [Sudden Stop (1G)] -> [<choice> Accel > 4G]
 [Smartphone Drop (3G)] -> [<choice> Accel > 4G]
