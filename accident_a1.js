bd6f2824d944bdc5205494a822772915a8191f61
Joseph Sungpil Choi <chousemath@gmail.com>
2018-12-12 13:30:07 +0900

    Quick save...

--- accident_a1.js
+++ accident_a1.js
@@ -4,11 +4,14 @@ const src = `
 [<choice> Vel > 15mph] -> [<abstract> No*]
 [<choice> Vel > 15mph] -> [<abstract> Yes*]
 [<abstract> No*] -> [Ignore]
-[<abstract> Yes*] ->[<choice> Accel > 4G]
+[<abstract> Yes*] -> [<choice> Accel > 4G]
 [<choice> Accel > 4G] -> [<abstract> No**]
 [<choice> Accel > 4G] -> [<abstract> Yes**]
 [<abstract> No**] -> [Ignore]
 [<abstract> Yes**] -> [Accident]
+
+[Sudden Stop (1G)] -> [<choice> Accel > 4G]
+[Smartphone Drop (3G)] -> [<choice> Accel > 4G]
 `;
 
 console.log(nomnoml.renderSvg(src));
