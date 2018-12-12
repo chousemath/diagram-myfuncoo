78485cd8fe7abf2acb3ab402b9f87b40d1efc096
Joseph Sungpil Choi <chousemath@gmail.com>
2018-12-12 13:50:54 +0900

    Quick save...

--- accident_a1.js
+++ accident_a1.js
@@ -5,11 +5,11 @@ const src = `
 ${styles._.yes}
 ${styles._.no}
 
-[<choice> Vel > 15mph] -> [<no> No*]
+[<choice> Vel > 15mph] --:> [<no> No*]
 [<choice> Vel > 15mph] -> [<yes> Yes*]
 [<no> No*] --:> [Ignore]
 [<yes> Yes*] -> [<choice> Accel > 4G]
-[<choice> Accel > 4G] -> [<no> No**]
+[<choice> Accel > 4G] --:> [<no> No**]
 [<choice> Accel > 4G] -> [<yes> Yes**]
 [<no> No**] --:> [Ignore]
 [<yes> Yes**] -> [Accident]
