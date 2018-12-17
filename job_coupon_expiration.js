bd8451ca79351101cda2becff62ec738eeb1efda
Joseph Sungpil Choi <chousemath@gmail.com>
2018-12-17 14:37:34 +0900

    Quick save...

---
+++ job_coupon_expiration.js
@@ -0,0 +1,27 @@
+const styles = require('./styles');
+const nomnoml = require('nomnoml');
+
+const src = `
+${styles._.header}
+
+${styles._.accident}
+${styles._.ignore}
+${styles._.condition}
+${styles._.yes}
+${styles._.no}
+
+[ Active Coupons
+  | Status is active
+  | EndsAt != 0
+  | EndsAt <= now
+  ]
+  
+[Active Coupons] -> [<accident> Set to inactive]
+[<accident> Set to inactive] -> [<accident> Set EndsAt to 0]
+[<accident> Set EndsAt to 0] -> [<accident> Set UpdatedAt to now]
+[<accident> Set UpdatedAt to now] -> [<ignore> Wait 1 min]
+[Wait 1 min] -> [<condition> Get active coupons]
+[<condition> Get active coupons] -> [Active Coupons]
+`;
+
+console.log(nomnoml.renderSvg(src));
