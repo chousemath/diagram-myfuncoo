2284da2f82aa0b5e98e04b043572fe8b3d145c9b
Joseph Sungpil Choi <chousemath@gmail.com>
2018-12-17 17:10:25 +0900

    Quick save...

--- job_coupon_category_reward_expiration.js
+++ job_coupon_category_reward_expiration.js
@@ -10,16 +10,16 @@ ${styles._.condition}
 ${styles._.yes}
 ${styles._.no}
 
-[ Active Coupons
+[ Active CouponCategoryRewards
   | Status is active
   | EndsAt <= now
   ]
   
-[Active Coupons] -> [<accident> Set Status to inactive]
+[Active CouponCategoryRewards] -> [<accident> Set Status to inactive]
 [<accident> Set Status to inactive] -> [<accident> Set UpdatedAt to now]
 [<accident> Set UpdatedAt to now] -> [<ignore> Wait 1 min]
-[Wait 1 min] -> [<condition> Get active coupons]
-[<condition> Get active coupons] -> [Active Coupons]
+[Wait 1 min] -> [<condition> Get Active CouponCategoryRewards]
+[<condition> Get Active CouponCategoryRewards] -> [Active CouponCategoryRewards]
 `;
 
 console.log(nomnoml.renderSvg(src));
